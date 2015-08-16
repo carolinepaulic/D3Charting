c0141f89c47bee6c1e4929c2b8505b1987032aff
Caroline Paulic <carolinepaulic@eworldes.com>
2015-08-16 12:17:44 -1000

    Cleaned up d3 bar chart code

--- src/app/modules/barChart/BarChart.js
+++ src/app/modules/barChart/BarChart.js
@@ -1,18 +1,14 @@
 window.onload = function() {
-
     var data = [4, 8, 15, 16, 23, 42];
 
-    var chart = d3.select(".chart");
-    var bar = chart.selectAll("div");
-    var barUpdate = bar.data(data);
     var x = d3.scale.linear()
         .domain([0, d3.max(data)])
-        .range([0, 420]);
-    var barEnter = barUpdate.enter().append("div");
-    barEnter.style("width", function (d) {
-        return x(d) + "px";
-    });
-    barEnter.text(function (d) {
-        return d;
-    });
+        .range([0, 10 * d3.max(data)]);
+
+    d3.select(".chart")
+        .selectAll("div")
+            .data(data)
+        .enter().append("div")
+            .style("width", function(d) { return x(d) + "px"; })
+            .text(function (d) { return d; })
 };
