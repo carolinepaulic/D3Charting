ede2a2e621c3d18c54ad47855d1fdf4d33391158
Caroline Paulic <carolinepaulic@eworldes.com>
2015-08-16 13:29:59 -1000

    Used d3.json to grab data from file instead of declaring it

--- src/app/modules/barChart/BarChart.js
+++ src/app/modules/barChart/BarChart.js
@@ -1,27 +1,31 @@
 window.onload = function() {
-    var data = [4, 8, 15, 16, 23, 42];
-    var width = 10 * d3.max(data);
+    d3.json("modules/data/1ValueData.json", function(error, response) {
+        var data = response.data;
+        console.info(data);
 
-    var x = d3.scale.linear()
-        .domain([0, d3.max(data)])
-        .range([0, width]);
+        var width = 10 * d3.max(data);
 
-    /* Used for SVG chart */
-    var barHeight = 20;
+        var x = d3.scale.linear()
+            .domain([0, d3.max(data)])
+            .range([0, width]);
 
-    var chart = d3.select(".svg-chart")
-        .attr("width", width)
-        .attr("height", barHeight * data.length);
-    var bar = chart.selectAll("g")
+        /* Used for SVG chart */
+        var barHeight = 20;
+
+        var chart = d3.select(".svg-chart")
+            .attr("width", width)
+            .attr("height", barHeight * data.length);
+        var bar = chart.selectAll("g")
             .data(data)
-        .enter().append("g")
+            .enter().append("g")
             .attr("transform", function(d,i) { return "translate(0," + i * barHeight + ")"; }); //d is data and i is index?
-    bar.append("rect")
-        .attr("width", x)
-        .attr("height", barHeight - 1);
-    bar.append("text")
-        .attr("x", function(d) { return x(d) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d; });
+        bar.append("rect")
+            .attr("width", x)
+            .attr("height", barHeight - 1);
+        bar.append("text")
+            .attr("x", function(d) { return x(d) - 3; })
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .text(function(d) { return d; });
+    });
 };
